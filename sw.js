f89888fc9ed0542e2a404e27a514c4cc4a8165bb
Henrique Lima <henrique.souza.lima@outlook.com>
2026-09-11 19:53:12 -0300

    chore: bump service worker cache to v2

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Caching Application Shell
 
-const CACHE_NAME = 'sandypetshop-cache-v1';
+const CACHE_NAME = 'sandypetshop-cache-v2';
 const urlsToCache = [
   '/',
   '/index.html',
